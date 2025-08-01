77e4d2a3767d5e8ba971c1e6ed4f19c4a7beecfc
Marvin Gonzalez <marvinjgh@gmail.com>
2025-07-31 21:52:59 -0400

    setting CreateAt and UpdateAt

--- Backend.Service/Models/Project.cs
+++ Backend.Service/Models/Project.cs
@@ -1,22 +1,23 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Backend.Service.Models;
 
-public class Project
+[Table("Project")]
+public class Project : BaseEntity
 {
+    [Key]
+    [Column("ProjectId")]
     public long Id { get; set; }
+
     [Required(ErrorMessage = "Name is required")]
-    [StringLength(64, ErrorMessage = "Name cannot be longer than 100 characters")]
+    [StringLength(64, ErrorMessage = "Name cannot be longer than 64 characters")]
     public string? Name { get; set; }
 
     public string? Description { get; set; }
 
-    public DateTime CreateAt { get; set; }
-
-    public DateTime? UpdateAt { get; set; }
-
     public override string ToString()
     {
-        return $"Project [Id: {Id}, Name: {Name ?? "N/A"}, Description: {Description ?? "N/A"}, CreateAt: {CreateAt}, UpdateAt: {(UpdateAt.HasValue ? UpdateAt.Value.ToString() : "N/A")}]";
+        return $"Project [Id: {Id}, Name: {Name ?? "N/A"}, Description: {Description ?? "N/A"}]";
     }
 }
--- Backend.Service/RepositoryContext.cs
+++ Backend.Service/RepositoryContext.cs
@@ -10,6 +10,48 @@ public class RepositoryContext : DbContext
     {
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+
+    public override int SaveChanges()
+    {
+        var entries = ChangeTracker.Entries();
+        var now = DateTime.UtcNow;
+
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added && entry.Metadata.FindProperty("CreatedAt") != null)
+            {
+                entry.Property("CreatedAt").CurrentValue = now;
+            }
+            else if (entry.State == EntityState.Modified && entry.Metadata.FindProperty("UpdatedAt") != null)
+            {
+                entry.Property("UpdatedAt").CurrentValue = now;
+            }
+        }
+        return base.SaveChanges();
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries();
+        var now = DateTime.UtcNow;
+
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added && entry.Metadata.FindProperty("CreatedAt") != null)
+            {
+                entry.Property("CreatedAt").CurrentValue = now;
+            }
+            else if (entry.State == EntityState.Modified && entry.Metadata.FindProperty("UpdatedAt") != null)
+            {
+                entry.Property("UpdatedAt").CurrentValue = now;
+            }
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
     public DbSet<Project> TodoItems { get; set; }
 }
 
